3ff825ce1217091c6e6543b9dd93bb4bba20ec6a
natasa118 <balognatasa118@gmail.com>
2025-05-28 21:14:18 +0200

    Add files via upload
    prva verzija

---
+++ Test3.py
@@ -0,0 +1,19 @@
+import os
+
+print(os.getcwd())
+
+os.chdir("/home/natasa/Desktop/python")
+
+fajl = open("test3Fajl.txt", "w")
+fajl.write("fajl je upravo napravljen\n")
+fajl.close()
+
+fajl = open("test3Fajl.txt","a")
+fajl.write("prvi dodat red\n")
+fajl.write("drugi dodat red\n")
+fajl.close()
+
+
+with open("test3Fajl.txt","r") as f:
+    for linija in f:
+        print(linija.strip())
